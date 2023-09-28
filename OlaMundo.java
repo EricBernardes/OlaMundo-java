850567818e044e33af2bab0d6c914a93a731b51d
EricBernardes <ericbernardes70@gmail.com>
2023-09-28 03:00:12 -0300

    first commit

---
+++ OlaMundo.java
@@ -0,0 +1,15 @@
+public class OlaMundo{
+	public static void main(String[] args){
+		System.out.println("Ola Mundo!!!");
+	}
+}
+
+/*
+Nome da classe iniciam com Maiuscula
+variaveis: minusculas e palavras concatenadas iniciam em maiusculas
+nomeAlunoGraduacao
+
+nomeDoMetodo()
+
+ID_ALUNO - nome das contantes são separadas por "_"
+*/
